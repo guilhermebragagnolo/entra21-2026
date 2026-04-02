7c19d27793d09ea192ec02390622534089fda66b
Guilherme Cunha <guilherme63874456@alunos.sc.senac.br>
2026-04-02 14:52:42 -0300

    Mais alguns exercicios realizados

--- src/DesvioCondicional/Lista2.java
+++ src/DesvioCondicional/Lista2.java
@@ -7,10 +7,13 @@ public class Lista2 {
 	public static void main(String[] args) {
 		// exercicio1();
 		// exercicio2();
-		exercicio3();
-
+		//exercicio3();
+		//exercicio4();
+		//exercicio5();
+		exercicio6();
+		
 	}
-
+	//Nível básico
 	static void exercicio1() {
 		Scanner sc = new Scanner(System.in);
 
@@ -58,5 +61,50 @@ public class Lista2 {
 			System.out.println("O número é negativo");
 		}
 
+	}
+	static void exercicio4() {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Informe a sua nota do boletim: ");
+		double nota = sc.nextDouble();
+		
+		if (nota >= 6) {
+			System.out.println("Você foi aprovado!");
+		}else {
+			System.out.println("Você foi reprovado :(");
+		}
+	}
+	
+	static void exercicio5() {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Informe sua idade: ");
+		int idade = sc.nextInt();
+		
+		if (idade >= 16) {
+			System.out.println("Você pode votar!");
+		}else {
+			System.out.println("Você ainda não pode votar!");
+		}
+	}
+	static void exercicio6() {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Informe o primeiro número: ");
+		int numero1 = sc.nextInt();
+		
+		System.out.println("Informe o segundo número: ");
+		int numero2 = sc.nextInt();
+		
+		System.out.println("Informe o terceiro número: ");
+		int numero3 = sc.nextInt();
+		
+		if (numero1 > numero2 && numero1 > numero3) {
+			System.out.println("O primeiro número é o maior");
+		}else if (numero2 > numero1 && numero2 > numero3) {
+			System.out.println("O segundo número é o maior");
+		}else {
+			System.out.println("O terceiro número é o maior");
+		}
 	}
 }
